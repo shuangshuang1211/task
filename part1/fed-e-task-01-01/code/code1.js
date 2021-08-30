d950aca784d4ab6f6291f97afdbb2f83133fa74a
shuangshuang <44526628+shuangshuang1211@users.noreply.github.com>
2021-08-30 21:21:14 +0800

    docs: modify code1 task

--- part1/fed-e-task-01-01/code/code1.js
+++ part1/fed-e-task-01-01/code/code1.js
@@ -26,3 +26,17 @@ const p3 = p2.then((value2) => {
   setTimeout(console.log, 10, `${value2}I ♥ U`);
 });
 
+
+// 方法2
+async function asyncFn () {
+  // console.log(new Date().getTime());
+  const p1 = await promise('hello');
+  // console.log(new Date().getTime());
+  const p2 = await promise('lagou');
+  // console.log(new Date().getTime());
+  const p3 = await promise('I ♥ U');
+  console.log(p1 + p2 + p3);
+}
+
+asyncFn();
+
